021a77e03d096e5c81c161db122db5f6b4bf5677
rahul-jfs <ra1290hul@gmail.com>
2025-02-26 23:48:56 +0530

    useRef hook

--- projects/05-Todo-app-Version2/src/components/AddTodo.jsx
+++ projects/05-Todo-app-Version2/src/components/AddTodo.jsx
@@ -1,51 +1,35 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 function AddTodo({ handleAddButton }) {
-  const [todoName, setTodoName] = useState("");
-  const [dueDate, setDueDate] = useState("");
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
 
-  const handleTodoName = (event) => {
-    setTodoName(event.target.value);
-  };
-
-  const handleDueDate = (event) => {
-    setDueDate(event.target.value);
-  };
-
-  const addBtnClick = () => {
+  const addBtnClick = (event) => {
+    event.preventDefault();
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
     handleAddButton(todoName, dueDate);
-    setTodoName("");
-    setDueDate("");
   };
 
   return (
     <div className="container text-center">
-      <div className="row kg-row">
+      <form className="row kg-row" onSubmit={addBtnClick}>
         <div className="col-6">
           <input
             type="text"
             placeholder="Enter Todo Here"
-            value={todoName}
-            onChange={(event) => handleTodoName(event)}
+            ref={todoNameElement}
           />
         </div>
         <div className="col-4">
-          <input
-            type="date"
-            value={dueDate}
-            onChange={() => handleDueDate(event)}
-          />
+          <input type="date" ref={dueDateElement} />
         </div>
         <div className="col-2">
-          <button
-            type="button"
-            className="btn btn-success kg-button"
-            onClick={addBtnClick}
-          >
+          <button type="submit" className="btn btn-success kg-button">
             Add
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
